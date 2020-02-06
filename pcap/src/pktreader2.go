dc9b67233b4145c9923e54bc25ddd9786568a3da
ggmartins <ggmartins@gmail.com>
2020-02-06 14:19:34 -0600

    feat: pcap reflect cleanup

--- pcap/src/pktreader2.go
+++ pcap/src/pktreader2.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/google/gopacket"
-	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 	//"github.com/google/gopacket/pfring"
 )
@@ -42,91 +41,61 @@ func main() {
 		var i uint8
 		var d uint8
 
-		/*s := reflect.ValueOf(packet).Elem()
-		typeOfT := s.Type()
-		for i := 0; i < s.NumField(); i++ {
-			f := s.Field(i)
-			fmt.Printf("%d: %s %s = %v\n", i,
-				typeOfT.Field(i).Name, f.Type(), f.Interface())
-		}*/
-
-		if ethLayer := packet.Layer(layers.LayerTypeEthernet); ethLayer != nil {
-			eth, _ := ethLayer.(*layers.Ethernet)
-			fmt.Printf("[%d] NextLayerType\n", eth.NextLayerType())
-		}
-
-		if ipv4Layer := packet.Layer(layers.LayerTypeIPv4); ipv4Layer != nil {
-			ipv4, _ := ipv4Layer.(*layers.IPv4)
-			fmt.Printf("IP[%d,%d][%d]\n", ipv4.SrcIP, ipv4.DstIP, ipv4.NextLayerType())
-			//i = ipv4.IHL
-
-			/*s := reflect.ValueOf(ipv4).Elem()
+		fmt.Printf("Packet: %s\n", packet.String()) // packet.Layers())
+		pktlayers := packet.Layers()
+		for ind, pktlayer := range pktlayers {
+			s := reflect.ValueOf(pktlayer).Elem()
 			typeOfT := s.Type()
-			for i := 0; i < s.NumField(); i++ {
+			typeName := fmt.Sprintf("%s", typeOfT)
+			fmt.Printf("%d> %s, %s\n", ind, pktlayer.LayerType(), typeOfT)
+			if typeName != "gopacket.Payload" {
+				for i := 0; i < s.NumField(); i++ {
+					f := s.Field(i)
+					layerName := fmt.Sprintf("%s", typeOfT.Field(i).Name)
+					if layerName == "BaseLayer" {
+						continue
+					}
+					//non-private field
+					if (layerName[0] > 64) && (layerName[0] < 91) {
+						fmt.Printf("%d:", i)
+						fmt.Printf(" %s", typeOfT.Field(i).Name)
+						fmt.Printf(" [%s]", f.Type())
+						fmt.Printf(" = %v\n", f.Interface())
+					}
+				}
+			} else {
+				if app := packet.ApplicationLayer(); app != nil {
+					for _, b := range app.Payload() {
+						fmt.Printf("%02x:", b)
+					}
+				}
+				fmt.Printf("\n")
 
-				f := s.Field(i)
-				layer := fmt.Sprintf("%s", typeOfT.Field(i).Name)
-				if layer == "BaseLayer" {
-					continue
-				}
-				fmt.Printf("%d: %s [%s] = %v\n", i,
-					typeOfT.Field(i).Name, f.Type(), f.Interface())
-			}*/
-		}
-
-		if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
-			tcp, _ := tcpLayer.(*layers.TCP)
-			fmt.Printf("TCP[%d,%d]", tcp.SrcPort, tcp.DstPort)
-			d = tcp.DataOffset
-			s := reflect.ValueOf(udp).Elem()
-			typeOfT := s.Type()
-			for i := 0; i < s.NumField(); i++ {
-				f := s.Field(i)
-				layerName := fmt.Sprintf("%s", typeOfT.Field(i).Name)
-				if layerName == "BaseLayer" {
-					continue
-				}
-				//non-private field
-				if (layerName[0] > 64) || (layerName[0] < 91) {
-					fmt.Printf("%d:", i)
-					fmt.Printf(" %s", typeOfT.Field(i).Name)
-					fmt.Printf(" [%s]", f.Type())
-					fmt.Printf(" = %v\n", f.Interface())
-				}
 			}
 		}
-
-		if udpLayer := packet.Layer(layers.LayerTypeUDP); udpLayer != nil {
-			udp, _ := udpLayer.(*layers.UDP)
-			fmt.Printf("UDP[%d,%d]\n", udp.SrcPort, udp.DstPort)
-			s := reflect.ValueOf(udp).Elem()
-			typeOfT := s.Type()
-			for i := 0; i < s.NumField(); i++ {
-				f := s.Field(i)
-				layerName := fmt.Sprintf("%s", typeOfT.Field(i).Name)
-				if layerName == "BaseLayer" {
-					continue
-				}
-				//non-private field
-				if (layerName[0] > 64) || (layerName[0] < 91) {
-					fmt.Printf("%d:", i)
-					fmt.Printf(" %s", typeOfT.Field(i).Name)
-					fmt.Printf(" [%s]", f.Type())
-					fmt.Printf(" = %v\n", f.Interface())
-				}
+		//os.Exit(1)
+		/*
+			if ethLayer := packet.Layer(layers.LayerTypeEthernet); ethLayer != nil {
+				eth, _ := ethLayer.(*layers.Ethernet)
+				fmt.Printf("[%d] NextLayerType\n", eth.NextLayerType())
 			}
-		}
 
-		if app := packet.ApplicationLayer(); app != nil {
-			//mt.Printf("" + string(app.Payload()))
-			//s := reflect.ValueOf(app.Payload()).Elem()
-			//typeOfT := s.Type()
-			//for i := 0; i < s.NumField(); i++ {
-			//	f := s.Field(i)
-			//	fmt.Printf("%d: %s %s = %v\n", i,
-			//		typeOfT.Field(i).Name, f.Type(), f.Interface())
-			//}
-		}
+			if ipv4Layer := packet.Layer(layers.LayerTypeIPv4); ipv4Layer != nil {
+				ipv4, _ := ipv4Layer.(*layers.IPv4)
+				fmt.Printf("IP[%d,%d][%d]\n", ipv4.SrcIP, ipv4.DstIP, ipv4.NextLayerType())
+			}
+
+			if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+				tcp, _ := tcpLayer.(*layers.TCP)
+				fmt.Printf("TCP[%d,%d]\n", tcp.SrcPort, tcp.DstPort)
+			}
+
+			if udpLayer := packet.Layer(layers.LayerTypeUDP); udpLayer != nil {
+				udp, _ := udpLayer.(*layers.UDP)
+				fmt.Printf("UDP[%d,%d]\n", udp.SrcPort, udp.DstPort)
+			}
+		*/
+
 		o := uint32((i + d) * 4)
 		fmt.Printf(" %d\n", o)
 	}
